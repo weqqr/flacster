61fb5fa04d6dbd19cf176800eb33c20e8f317616
UnTea <weqqu2014@yandex.ru>
2024-01-31 22:29:48 +0300

    rollback configuration files

---
+++ config.mk
@@ -0,0 +1,8 @@
+POSTGRES_USERNAME = postgres_username
+POSTGRES_OWNER = postgres_owner
+DB_NAME = db_name
+CONTAINER_NAME = container_name
+DOCKER_COMPOSE = docker-compose.yml
+DOCKER_COMPOSE_OVERRIDE = docker-compose.override.yml
+MIGRATION_PATH = internal/db/migration
+DB_CONNECTION_STRING = postgresql://user:password@localhost:port/db?sslmode=disable
