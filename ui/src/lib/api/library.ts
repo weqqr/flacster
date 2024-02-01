e177967e3afe899b93d095fee657e2f3016e84b9
weqqr <weqqr@yandex.ru>
2024-02-01 22:14:40 +0300

    Set up proxy to local API

--- ui/src/lib/api/library.ts
+++ ui/src/lib/api/library.ts
@@ -4,7 +4,7 @@ async function request(endp: string): Promise<any> {
 }
 
 export async function listFiles(): Promise<string[]> {
-    const response = await request('http://localhost:1337/api/v1/library/files');
+    const response = await request('/api/v1/library/files');
     return response['files'].map((x: string) => {
         const parts = x.split(/\\|\//);
         return parts[parts.length - 1];
--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -2,5 +2,15 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		proxy: {
+			"/api": {
+				target: "http://localhost:1337",
+				changeOrigin: true,
+				secure: false,
+				ws: true,
+			},
+		},
+	},
 });
